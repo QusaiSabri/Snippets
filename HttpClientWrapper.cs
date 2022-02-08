9145cc273a6db9d94fa786c351bc12e4a1d5e82a
Qusai Sabri <qusai.sabri19@gmail.com>
2022-02-08 16:19:42 -0600

    Create HttpClientWrapper.cs

---
+++ HttpClientWrapper.cs
@@ -0,0 +1,64 @@
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+
+namespace services
+{
+    public class HttpClientWrapper<T> where T : class
+    {
+        public static async Task<T> Get(string url)
+        {
+            T result = null;
+            using (var httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(new Uri(url)).Result;
+
+                response.EnsureSuccessStatusCode();
+                await response.Content.ReadAsStringAsync().ContinueWith((Task<string> x) =>
+                {
+                    if (x.IsFaulted)
+                        throw x.Exception;
+
+                    result = JsonConvert.DeserializeObject<T>(x.Result);
+                });
+            }
+
+            return result;
+        }
+
+        public static async Task<T> PostRequest(string apiUrl, T postObject)
+        {
+            T result = null;
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(apiUrl, postObject, new JsonMediaTypeFormatter()).ConfigureAwait(false);
+
+                response.EnsureSuccessStatusCode();
+
+                await response.Content.ReadAsStringAsync().ContinueWith((Task<string> x) =>
+                {
+                    if (x.IsFaulted)
+                        throw x.Exception;
+
+                    result = JsonConvert.DeserializeObject<T>(x.Result);
+
+                });
+            }
+
+            return result;
+        }
+
+        public static async Task PutRequest(string apiUrl, T putObject)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PutAsync(apiUrl, putObject, new JsonMediaTypeFormatter()).ConfigureAwait(false);
+
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
